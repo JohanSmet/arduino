4ea902c1d324abfdcdfd6cdcb29ae51960feee2e
Johan Smet <johan.smet@justcode.be>
2021-11-13 17:51:44 +0100

    controller_sega: extended to support MegaDrive controllers.

--- controller_sega/controller_sega.ino
+++ controller_sega/controller_sega.ino
@@ -24,7 +24,12 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Based upon information published by Jon Thysell at
+ * https://github.com/jonthysell/SegaController/wiki/How-To-Read-Sega-Controllers
  */
+
+/* Please NOTE: not tested with a 6-button MegaDrive controller; I do not currently have one available. */
 
 /* Arduino pin definitions */
 const uint8_t PIN_DB1 = 22;
@@ -38,14 +43,28 @@ const uint8_t PIN_DB7 = 32;
 const uint8_t PIN_DB9 = 34;
 
 /* controller button flags */
+const uint16_t BTN_NONE  = 0;
 const uint16_t BTN_UP	 = 1 << 0;
 const uint16_t BTN_DOWN  = 1 << 1;
 const uint16_t BTN_LEFT  = 1 << 2;
 const uint16_t BTN_RIGHT = 1 << 3;
-const uint16_t BTN_A	 = 1 << 4;
-const uint16_t BTN_B	 = 1 << 5;
+const uint16_t BTN_B	 = 1 << 4;
+const uint16_t BTN_C	 = 1 << 5;
+const uint16_t BTN_A	 = 1 << 6;
+const uint16_t BTN_X	 = 1 << 7;
+const uint16_t BTN_Y	 = 1 << 8;
+const uint16_t BTN_Z	 = 1 << 9;
+const uint16_t BTN_START = 1 << 10;
+const uint16_t BTN_MODE  = 1 << 11;
 
-const uint8_t  MS_BTN_COUNT = 6;	// Sega Master System
+const uint16_t BTN_MD3   = 1 << 14;		// flags to indicate that either a MegaDrive/Genesis
+const uint16_t BTN_MD6   = 1 << 15;		//		3-button or 6-button controller was detected
+
+const uint16_t MASK_CONTROLLER = 0xC000;
+const uint16_t MASK_BUTTONS	   = 0x3fff;
+
+const uint8_t  MS_BTN_COUNT = 6;		// Sega Master System
+const uint8_t  MD_BTN_COUNT = 12;		// Sega MegaDrive/Genesis (6-button controller)
 
 const char *MS_BTN_NAMES[MS_BTN_COUNT] = {
 	"Up",
@@ -54,6 +73,21 @@ const char *MS_BTN_NAMES[MS_BTN_COUNT] = {
 	"Right",
 	"1/Start",
 	"2"
+};
+
+const char *MD_BTN_NAMES[MD_BTN_COUNT] = {
+	"Up",
+	"Down",
+	"Left",
+	"Right",
+	"B",
+	"C",
+	"A",
+	"X",
+	"Y",
+	"Z",
+	"Start",
+	"Mode"
 };
 
 /* input mappings */
@@ -67,27 +101,131 @@ sega_ms_input_t ms_input[] = {
 	{ PIN_DB2, BTN_DOWN },
 	{ PIN_DB3, BTN_LEFT },
 	{ PIN_DB4, BTN_RIGHT },
-	{ PIN_DB6, BTN_A },
-	{ PIN_DB9, BTN_B },
+	{ PIN_DB6, BTN_B },
+	{ PIN_DB9, BTN_C },
+};
+
+typedef struct {
+	uint8_t		pin;
+	uint16_t	button[3];
+} sega_md_input_t;
+
+sega_md_input_t md_input[] = {
+	{ PIN_DB1, {BTN_NONE,	BTN_UP,		BTN_Z}},
+	{ PIN_DB2, {BTN_NONE,	BTN_DOWN,	BTN_Y}},
+	{ PIN_DB3, {BTN_NONE,	BTN_LEFT,	BTN_X}},
+	{ PIN_DB4, {BTN_NONE,	BTN_RIGHT,	BTN_MODE}},
+	{ PIN_DB6, {BTN_A,		BTN_B,		BTN_NONE}},
+	{ PIN_DB9, {BTN_START,	BTN_C,		BTN_NONE}},
 };
 
 /* global variables */
 static uint16_t current_state = 0;
 
 /* helper functions */
-void print_state(uint16_t state) {
-	if (state == 0) {
-		return;
-	}
-
-	for (uint8_t btn = 0; btn < MS_BTN_COUNT; ++btn) {
+void print_state_buttons(uint16_t state, uint8_t count, const char **names) {
+	for (uint8_t btn = 0; btn < count; ++btn) {
 		if (state & (1 << btn)) {
-			Serial.print(MS_BTN_NAMES[btn]);
+			Serial.print(names[btn]);
 			Serial.print(" ");
 		}
 	}
+}
+
+void print_state(uint16_t state) {
+
+	if ((state & MASK_CONTROLLER) != (current_state & MASK_CONTROLLER)) {
+		if (state & BTN_MD6) {
+			Serial.println("Sega MegaDrive/Genesis 6-button controller detected");
+		} else if (state & BTN_MD3) {
+			Serial.println("Sega MegaDrive/Genesis 3-button controller detected");
+		} else {
+			Serial.println("Sega MasterSystem or no controller present");
+		}
+	}
+
+	if ((state & MASK_BUTTONS) == 0) {
+		return;
+	}
+
+	if (state & BTN_MD3) {
+		print_state_buttons(state, MD_BTN_COUNT, MD_BTN_NAMES);
+	} else {
+		print_state_buttons(state, MS_BTN_COUNT, MS_BTN_NAMES);
+	}
 
 	Serial.println();
+}
+
+uint16_t read_master_system_controller() {
+	uint16_t state = 0;
+
+	for (int idx = 0; idx < sizeof(ms_input) / sizeof(ms_input[0]); ++idx) {
+		if (digitalRead(ms_input[idx].pin) == LOW) {
+			state |= ms_input[idx].button;
+		}
+	}
+
+	return state;
+}
+
+uint16_t read_megadrive_controller(int phase) {
+	uint16_t state = 0;
+
+	for (int idx = 0; idx < sizeof(md_input) / sizeof(md_input[0]); ++idx) {
+		if (digitalRead(md_input[idx].pin) == LOW) {
+			state |= md_input[idx].button[phase];
+		}
+	}
+
+	return state;
+}
+
+uint16_t read_controller() {
+
+	// CYCLE 1 - SELECT LOW
+	digitalWrite(PIN_DB7, LOW);
+
+	// >> pins 3 & 4 are low when a MegaDrive/Genesis controller (either 3 or 6 buttons) is connected
+	bool md_present = !digitalRead(PIN_DB3) && !digitalRead(PIN_DB4);
+
+	if (!md_present) {
+		// either a MasterSystem or no controller connected
+		return read_master_system_controller();
+	}
+
+	// CYCLE 1 - SELECT HIGH
+	digitalWrite(PIN_DB7, HIGH);
+
+	// CYCLE 2 - SELECT LOW
+	digitalWrite(PIN_DB7, LOW);
+	uint16_t state = BTN_MD3 | read_megadrive_controller(0);
+
+	// CYCLE 2 - SELECT HIGH
+	digitalWrite(PIN_DB7, HIGH);
+	state |= read_megadrive_controller(1);
+
+	// CYCLE 3 - SELECT LOW
+	digitalWrite(PIN_DB7, LOW);
+
+	// >> pins 1 & 2 are low when a MegaDrive/Genesis 6 button controller is connected
+	bool md6_present = !digitalRead(PIN_DB1) && !digitalRead(PIN_DB2);
+
+	if (!md6_present) {
+		return state;
+	}
+
+	// CYCLE 3 - SELECT HIGH
+	digitalWrite(PIN_DB7, HIGH);
+	state |= BTN_MD6 | read_megadrive_controller(3);
+
+	// CYCLE 4 - SELECT LOW
+	digitalWrite(PIN_DB7, LOW);
+
+	// CYCLE 4 - SELECT HIGH
+	digitalWrite(PIN_DB7, HIGH);
+
+	return state;
 }
 
 /* device setup */
@@ -109,13 +247,7 @@ void setup() {
 
 /* main loop */
 void loop() {
-	uint16_t state = 0;
-
-	for (int idx = 0; idx < sizeof(ms_input) / sizeof(ms_input[0]); ++idx) {
-		if (digitalRead(ms_input[idx].pin) == LOW) {
-			state |= ms_input[idx].button;
-		}
-	}
+	uint16_t state = read_controller();
 
 	if (state != current_state) {
 		print_state(state);
